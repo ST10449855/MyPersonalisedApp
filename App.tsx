76285da7fcff4b05512f60c2760f5a4a09e94f58
ST10449855 <163865994+ST10449855@users.noreply.github.com>
2024-08-01 13:51:36 +0200

    Application Development Complete

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.ViewStyle}>
+      <Text style={styles.headingText}>My Hobbies:</Text>
+      <Text style={styles.subHeading}>1. Soccer</Text>
+      <Text style={styles.subHeading}>2. Painting</Text>
+      <Text style={styles.subHeading}>3. Gaming</Text>
+      <Text style={styles.subHeading}>4. Swimming</Text>
+      <Text style={styles.subHeading}>5. Boxing</Text>
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  ViewStyle: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#90ee90',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  headingText: {
+    fontSize: 40,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  subHeading: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#ffc2cd',
+  }
 });
